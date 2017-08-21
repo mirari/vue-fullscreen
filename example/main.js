822a8d04f3e803fbfd5e1fc59b321cff3300ad48
mirari <clover4@gmail.com>
2017-08-21 20:58:35 +0800

    bug fix

--- example/main.js
+++ example/main.js
@@ -1,10 +1,9 @@
-import Vue from 'vue/dist/vue.min.js'
+import Vue from 'vue'
 import App from './App.vue'
 
 /* eslint-disable */
 console.log(Vue.version)
 new Vue({
-  el: '#app-wrap',
-  render: h => h(App)
-  // components: { App }
+  el: '#app',
+  ...App
 })
