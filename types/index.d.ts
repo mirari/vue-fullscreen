3ed64ea23a2a6abcaacb232130ef37cd7b911dcc
Cameron Clark <cameron.clark@hey.com>
2021-04-06 14:29:15 +0100

    docs(type file): add jsdoc for index.d.ts (types) file

--- types/index.d.ts
+++ types/index.d.ts
@@ -8,14 +8,36 @@ type VueFullScreenOptions = {
   background?: string;
 };
 declare class VueFullscreen {
+  /**
+   * Attempts to toggle fullscreen using the target element
+   *
+   * @param target - Target element to enter fullscreen, defaults to `document.body`
+   * @param options - The fullscreen options
+   * @param force - `true` to force enter, `false` to exit fullscreen
+   */
   toggle(
     target?: Element,
     options?: VueFullScreenOptions,
     force?: boolean | undefined
   ): void;
+  /**
+   * Attempts to enter fullscreen using the target element
+   *
+   * @param target - Target element to enter fullscreen, defaults to `document.body`
+   * @param options - The fullscreen options
+   */
   enter(target?: Element, options?: VueFullScreenOptions): void;
+  /**
+   * Exits fullscreen
+   */
   exit(): void;
+  /**
+   * Gets the fullscreen state
+   */
   getState(): boolean;
+  /**
+   * Check browser support for the fullscreen API
+   */
   support: boolean;
 }
 
